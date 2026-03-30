f8a381425353d24797ad3b32f806904c86960282
Sathyamrit <sathya.amrit@gmail.com>
2026-03-31 02:42:54 +0530

    updated path

--- api/convert.py
+++ api/convert.py
@@ -53,7 +53,7 @@ async def post_convert_root(request: Request) -> Response:
     return await _convert(request)
 
 
-@app.post("/convert")
+@app.post("/api/convert")
 async def post_convert_path(request: Request) -> Response:
     return await _convert(request)
 
